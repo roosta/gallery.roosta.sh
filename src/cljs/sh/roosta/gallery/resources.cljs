f7704d49f11dee0e2c1420aa4ba6bacb393bc3ca
Daniel Berg <mail@roosta.sh>
2016-10-02 20:15:43 +0200

    Update sizes.

--- src/cljs/sh/roosta/gallery/resources.cljs
+++ src/cljs/sh/roosta/gallery/resources.cljs
@@ -12,17 +12,17 @@
    {:id 8 :w 819 :h 1386 :title "Post cover" :src "img/cover.jpg" :category :painting :desc "Conseptual cover for a comic book that was never finished"}
    {:id 9 :w 437 :h 736 :title "Kanal Session Design" :src "img/cs.jpg" :category :design :desc "One of several design suggestions for a skating event. Not used but I still like it"}
    {:id 10 :w 1164 :h 785 :title "Drainpipes" :src "img/drainpipes.jpg" :category :photo :desc "Dark room development"}
-   {:id 11 :w 1017 :h 723  :title "Dunes" :src "img/dunes.jpg" :category :painting :desc "Sand dunes abstracted"}
+   {:id 11 :w 1525 :h 1084 :title "Dunes" :src "img/dunes.jpg" :category :painting :desc "Sand dunes abstracted"}
    {:id 12 :w 376 :h 520 :title "Masked Pencil Sketch" :src "img/edn.jpg" :category :drawing :desc "More pencil drawings of a gass mask wearing figure"}
    {:id 13 :w 121 :h 104 :gif true :title "Pixel Explosion" :src "img/expl.gif" :category :game :desc "Explosion from an unfinished shmup game"}
    {:id 14 :w 184 :h 358 :title "Floating Island" :src "img/flisland.png" :category :game :desc "Floating island for an unfinished shmup game"}
    {:id 15 :w 1193 :h 929 :title "Development fluid experiment" :src "img/fluid.jpg" :category :photo :desc "Messing around with development fluid that turned out kinda neat"}
-   {:id 16 :w 711 :h 711  :title "Goat Boys LP cover design" :src "img/gbcover.jpg" :category :design :desc "Cover design for Goat Boys debut album" :link "https://open.spotify.com/artist/2GadG4cFR4bNH6fyLFt8JK"}
+   {:id 16 :w 2136 :h 2136 :title "Goat Boys LP cover design" :src "img/gbcover.jpg" :category :design :desc "Cover design for Goat Boys debut album" :link "https://open.spotify.com/artist/2GadG4cFR4bNH6fyLFt8JK"}
    {:id 17 :w 497 :h 635 :title "Ghost Guy" :src "img/ghost.jpg" :category :vector :desc "Early vector attempt at a ghost like creature inspired by anime/games and the masks ghosts wear"}
    {:id 18 :w 548 :h 320 :title "Gray Brother" :src "img/graybrother.jpg" :category :drawing :desc "Pencil drawing of a gass masked wearing person"}
    {:id 19 :w 1024 :h 576 :title "Hooded Moon" :src "img/hoodie.jpg" :category :design :desc "Cover suggestion for a band, that was never used"}
    {:id 20 :w 720 :h 470 :title "Hufda" :src "img/hufda.jpg" :category :design :desc "Cover suggestion for a band, it that was never used"}
-   {:id 21 :w 779 :h 1087  :title "Selv Portrait with background" :src "img/inkdanfull.jpg" :category :painting :desc "Selv portrait with a superimposed background"}
+   {:id 21 :w 779 :h 1087 :title "Selv Portrait with background" :src "img/inkdanfull.jpg" :category :painting :desc "Selv portrait with a superimposed background"}
    {:id 22 :w 625 :h 859 :title "Selv Portrait" :src "img/inkdans.jpg" :category :painting :desc "Selv portrait using ink on plastic"}
    {:id 23 :w 486 :h 566 :title "Insectlike" :src "img/insectlike.jpg" :category :drawing :desc "One of several inksect inspired mask drawings"}
    {:id 24 :w 609 :h 924 :title "Julie series nr 1" :src "img/js1.jpg" :category :photo :desc "First in a photo series taken during medieval festival and developed in a darkroom"}
@@ -38,7 +38,7 @@
    {:id 35 :w 480 :h 480 :title "Monoxieman Running" :src "img/monoxieman_running.gif" :category :game :desc "Running animation character sprite for unfinished game"}
    {:id 36 :w 592 :h 800 :title "More ink!" :src "img/moreink.jpg" :category :drawing :desc "Random sketch drawn with a rough hand"}
    {:id 37 :w 592 :h 800 :title "More mask!" :src "img/moremask.jpg" :category :drawing :desc "Random sketch drawn with a rough hand"}
-   {:id 38 :w 624 :h 788 :title "Nomad Angle" :src "img/nomad-ang.jpg" :category :painting :desc "Titular nomad drawn from an angle. Comic book concept"}
+   {:id 38 :w 1248 :h 1575 :title "Nomad Angle" :src "img/nomad-ang.jpg" :category :painting :desc "Titular nomad drawn from an angle. Comic book concept"}
    {:id 39 :w 638 :h 878 :title "Nomad Equipped" :src "img/nomad-eq.jpg" :category :drawing :desc "Nomad with equipment, comic book concept"}
    {:id 40 :w 592 :h 725 :title "Ouroboros" :src "img/ouroboros.jpg" :category :drawing :desc "Unfinished sketch of ouroboros"}
    {:id 41 :w 752 :h 696 :title "Pattern Composition" :src "img/pattern_comp.jpg" :category :drawing :desc "One of many patterns inspired loosely on the art nouveau style"}
@@ -46,9 +46,9 @@
    {:id 43 :w 1545 :h 380 :title "House gardener logo suggestion" :src "img/precore.png" :category :vector :desc "Logo suggestion for a design/gardening company. Not used but I still like it"}
    {:id 44 :w 598 :h 877 :title "Runny ink portrait" :src "img/riportrait.jpg" :category :painting :desc "Self portrait with gratuitous use of ink"}
    {:id 45 :w 610 :h 873 :title "Salt" :src "img/salt.jpg" :category :painting :desc "Salt/sand dunes abstracted illustration"}
-   {:id 46 :w 592 :h 800 :title "Sectered" :src "img/secterd.jpg" :category :painting :desc "Gass mask wearing kid in the series of insect inspired gass masks"}
+   {:id 46 :w 2368 :h 3200 :title "Sectered" :src "img/sectered.jpg" :category :painting :desc "Gass mask wearing kid in the series of insect inspired gass masks"}
    {:id 47 :w 851 :h 315 :title "Shmup Screenshot" :src "img/shmup.png" :category :game :desc "Composit unfinished shmup screenshot from an unfinished untitled game"}
-   {:id 48 :w 1019 :h 723 :title "Sea of sand" :src "img/sos.jpg" :category :painting :desc "Mountain surrounded by sand. Looks in hindsight more like water"}
+   {:id 48 :w 1527 :h 1084 :title "Sea of sand" :src "img/sos.jpg" :category :painting :desc "Mountain surrounded by sand. Looks in hindsight more like water"}
    {:id 49 :w 850 :h 1163 :title "Splotch" :src "img/splotch.jpg" :category :painting :desc "Watercolor and ink drawing of a gassmask wearing creature"}
    {:id 50 :w 779 :h 566 :title "Stones study" :src "img/stones.jpg" :category :painting :desc "Study of stone texture, watercolor"}
    {:id 51 :w 872 :h 461 :title "Thirst" :src "img/thirst.jpg" :category :painting :desc "Thirsty fellow drawn with ink on rice paper"}
