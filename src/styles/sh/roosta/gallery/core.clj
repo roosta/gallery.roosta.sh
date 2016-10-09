e994cb81c3561619aa1b3aa83a35b7cdaba4d621
Daniel Berg <mail@roosta.sh>
2016-10-09 20:04:18 +0200

    Add home style namespace.

--- src/styles/sh/roosta/gallery/core.clj
+++ src/styles/sh/roosta/gallery/core.clj
@@ -1,8 +1,10 @@
 (ns sh.roosta.gallery.core
   (:require [garden.def :refer [defstylesheet defstyles]]
+            [sh.roosta.gallery.home :as home]
             [garden.units :refer [px]]))
 
 ;; Change defstylesheet to defstyles.
 (defstyles screen
   [:body
-   {:background-color "white"}])
+   {:background-color "white"}]
+  home/screen)
---
+++ src/styles/sh/roosta/gallery/home.clj
@@ -0,0 +1,19 @@
+(ns sh.roosta.gallery.home
+  (:require [garden.def :refer [defstylesheet defstyles]]
+            [garden.color :as color :refer [hsl rgb]]
+            [garden.stylesheet :refer [at-media]]
+            [garden.units :as u :refer [px pt]]))
+
+(defstyles screen
+  [:.img-container
+   {:text-align "center"
+    :overflow "hidden"
+    }
+   [:img
+    {:position "relative"
+     :top "100%"
+     :left "100%"
+     :margin-top "-200%"
+     :margin-left "-200%"}]
+   ]
+  )
