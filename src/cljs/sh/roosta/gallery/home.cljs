99b56a667b5e058094be7bd4c178dae3255757ad
Daniel Berg <mail@roosta.sh>
2016-10-16 18:54:11 +0200

    Use seperate layouts per breakpoint.

--- src/cljs/sh/roosta/gallery/home.cljs
+++ src/cljs/sh/roosta/gallery/home.cljs
@@ -28,46 +28,70 @@
      items)))
 
 (defn Grid
-  [state]
-  (let [cols (:cols @state)]
-    (fn []
-      [ResponsiveGridLayout
-       {:className "layout"
-        ;; :layout layout
-        ;; :width (.-innerWidth js/window)
-        :isDraggable false
-        :on-breakpoint-change #(swap! state assoc :cols %2)
-        :isResizable false
-        :breakpoints {:lg 1200 :md 996 :sm 768 :xs 480 :xxs 0}
-        :cols {:lg 6 :md 4 :sm 2 :xs 2 :xxs 1}
-        :items 57
-        :margin [0 0]
-        ;; :cols 4
-        :rowHeight 100}
-       (map-indexed
-        (fn [index item]
-          ^{:key (str (:id item) "n")}
-          [:div.img-container {:data-grid {:x (mod index cols) :y 0 :w 1 :h (+ (rand-int 3) 2)}}
-           [:img {:src (:src item)}]
-           ])
-        resources/items)])))
+  [layout]
+  [ResponsiveGridLayout
+   {:className "layout"
+    :layouts (clj->js layout)
+    :isDraggable false
+    :isResizable false
+    :breakpoints {:lg 1200 :md 996 :sm 768 :xs 480 :xxs 0}
+    :cols {:lg 6 :md 4 :sm 2 :xs 2 :xxs 1}
+    :items 57
+    :margin [0 0]
+    :rowHeight 100}
+   (map-indexed
+    (fn [index item]
+      ^{:key (str (:id item) "n")}
+      [:div.img-container
+       [:img {:src (:src item)}]
+       ])
+    resources/items)])
 
 (defn Main
   []
-  (let [
-        items (transform-map resources/items)
-        gallery (js/PhotoSwipe.
+  (let [gallery (js/PhotoSwipe.
                  (dom/getElementByClass "pswp")
                  js/PhotoSwipeUI_Default
-                 items
+                 (transform-map resources/items)
                  #js {:index 0})
-        state (r/atom {:cols 6})]
+        layout {:lg
+                (reduce
+                 (fn [acc item]
+                   (conj acc
+                         {:i (str (:id item) "n")
+                          :x (mod (:id item) 6)
+                          :y 0
+                          :w 1
+                          :h (+ (rand-int 3) 2)}))
+                 []
+                 resources/items)
+                :md (reduce
+                     (fn [acc item]
+                       (conj acc
+                             {:i (str (:id item) "n")
+                              :x (mod (:id item) 4)
+                              :y 0
+                              :w 1
+                              :h (+ (rand-int 3) 2)}))
+                     []
+                     resources/items)
+                :sm (reduce
+                     (fn [acc item]
+                       (conj acc
+                             {:i (str (:id item) "n")
+                              :x (mod (:id item) 2)
+                              :y 0
+                              :w 1
+                              :h (+ (rand-int 3) 2)}))
+                     []
+                     resources/items)}]
     (r/create-class
      {
+      :component-will-mount #(d/log layout)
       ;; :component-did-mount #(init! vsm state)
       ;; :component-will-unmount #(.dispose vsm)
       :reagent-render
       (fn []
         [:div
-         [Grid state]]
+         [Grid layout]]
         )})))
