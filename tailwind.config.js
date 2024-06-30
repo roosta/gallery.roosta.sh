5b0928c8e1882a2197ca0a7bf3263cec39382fdf
Daniel Berg <mail@roosta.sh>
2024-06-30 17:51:22 +0200

    Add fonts (typeface + icons)
    Not quite settled on the combination yet, need some content to test out
    on, and I've just added it to the head for now. Consider downloading
    fonts when its been settled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,7 +7,12 @@ export default {
     "./main.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        work: ["Work Sans", ...defaultTheme.fontFamily.sans],
+        open: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
