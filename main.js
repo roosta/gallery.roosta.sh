5eb8e3e5e99784e01e2b235ea8a2dde0fe379895
Daniel Berg <mail@roosta.sh>
2024-07-04 15:10:52 +0200

    Add breakpoints comment

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ import flatten from "lodash/flatten";
 
 window.Alpine = Alpine;
 
+// const breakpoints = {
+//   sm: 640,
+//   md: 768,
+//   lg: 1024,
+//   xl: 1280,
+//   "2xl": 1536
+// }
+
 Alpine.data("assets", () => ({
   filtersOpen: false,
   data() {
