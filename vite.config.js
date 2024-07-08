b894f5117ae8f1d7f56b6d4d3c2cf6d34d70319a
Daniel Berg <mail@roosta.sh>
2024-07-08 14:50:11 +0200

    Pass assets as handlebars context

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-
 import handlebars from "vite-plugin-handlebars";
+import assets from "assets.json";
 
 export default {
-  plugins: [handlebars()],
+  plugins: [handlebars({
+    context: { assets }
+  })],
 };
