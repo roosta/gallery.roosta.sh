aa45ba5dca4e4a61ce39fd40d6961e4deba2b034
Daniel Berg <mail@roosta.sh>
2025-02-06 17:38:24 +0100

    Add about content

--- about.js
+++ about.js
@@ -2,6 +2,7 @@ import menu from "/menu.js"
 
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/brands';
 
 function main() {
   menu();
