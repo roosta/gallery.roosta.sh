1b0b5ca2964cb7019a6872b30673a2898bf9af01
Daniel Berg <mail@roosta.sh>
2025-02-17 18:33:39 +0100

    Move tailwind theme to style.css
    Need the conf file for content (partials), this fixes the issue with
    black causing issues when applying opacity on light theme (see menu)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 /** @type {import("tailwindcss").Config} */
 export default {
   content: [
@@ -7,28 +5,5 @@ export default {
     "*.html",
     "./partials/*"
   ],
-  darkMode: 'selector',
-  theme: {
-    extend: {
-      fontFamily: {
-        work: ["Work Sans", ...defaultTheme.fontFamily.sans],
-        open: ["Open Sans", ...defaultTheme.fontFamily.sans],
-      },
-      gridAutoRows: {
-        'gallery': 'minmax(12rem, 2fr)',
-      },
-      spacing: {
-        '128': '32rem',
-      },
-      colors: {
-        "white": "#E5E1D8",
-        "black": "#141414",
-        "dim-dark": "#A4A497",
-        "dim-light": "#484848",
-        "backdrop-dark": "#0D0D0D",
-        "backdrop-light": "#B4B1AB",
-      }
-    },
-  },
 }
 
